5ec18c10d8ceea5778839f07ed20cc78621519c4
Paweł Grudzień <p.grudzien12@gmail.com>
2018-02-20 18:01:44 +0100

    customer builder

---
+++ CapgeminiSample.Tests/CustomerBuilder.cs
@@ -0,0 +1,45 @@
+﻿using CapgeminiSample.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapgeminiSample.Tests
+{
+    class CustomerBuilder
+    {
+        private string Surname { get; set; } = "Thumbro";
+        private string Name { get; set; } = "of Glimmerworld";
+
+        private CustomerBuilder()
+        {
+        }
+
+        public CustomerBuilder WithName(string name)
+        {
+            Name = name;
+            return this;
+        }
+
+        public CustomerBuilder WithSurname(string surname)
+        {
+            Surname = surname;
+            return this;
+        }
+
+        public CustomerBuilder DefaultCustomer
+        {
+            get
+            {
+                return new CustomerBuilder();
+            }
+        }
+        public Customer Build()
+        {
+            return new Customer()
+            {
+                Name = Name,
+                Surname = Surname,
+            };
+        }
+    }
+}
